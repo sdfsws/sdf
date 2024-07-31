1649f9f2776597e800f65114a3707fcbc3293a21
Mohammed Alkabsh <163199327+sdfsws@users.noreply.github.com>
2024-07-31 23:50:02 +0300

    ok for now

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,14 +12,20 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Vite -->
     @vite('resources/js/app.js')
     @vite('resources/sass/app.scss')
 </head>
 <body>
-    <div id="app">
-        @yield('content')
+<div id="app">
+        <x-nav-menu />
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+
     </div>
 </body>
 </html>
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/sass/app.scss',
-                'resources/css/app.css',
                 'resources/js/app.js',
             ],
             refresh: true,
@@ -21,9 +20,7 @@ export default defineConfig({
             },
         }),
     ],
-    build: {
-        manifest: true,
-    },    
+
     resolve: {
         alias: {
             vue: 'vue/dist/vue.esm-bundler.js',
